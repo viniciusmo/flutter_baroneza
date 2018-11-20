b6268a683a01692dbc05bb65965a0819cba2dd7e
viniciusmo <viniciusoliveiravmo@gmail.com>
2018-11-20 21:06:26 -0200

    Add a line repository impl

---
+++ lib/src/data/LineRepositoryImpl.dart
@@ -0,0 +1,12 @@
+import 'package:baroneza/src/domain/Line.dart';
+import 'package:baroneza/src/domain/repository/LineRepository.dart';
+
+class LineRepositoryImpl implements LineRepository{
+
+  @override
+  Future<List<Line>> all() {
+    // TODO: implement all
+    return null;
+  }
+
+}
