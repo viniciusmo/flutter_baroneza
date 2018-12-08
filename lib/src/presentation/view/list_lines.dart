77c18de0d2ebd880c7d35edcce6ea9445d0af7d6
viniciusmo <viniciusoliveiravmo@gmail.com>
2018-12-08 16:04:12 -0200

    Add a listview

--- lib/src/presentation/view/list_lines.dart
+++ lib/src/presentation/view/list_lines.dart
@@ -11,35 +11,47 @@ class ListLines extends StatefulWidget {
 }
 
 class _ListLinesState extends State<ListLines> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: <Widget>[
-                  Image.network(
-                      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/CPTM_icon.svg/1024px-CPTM_icon.svg.png",
-                      width: 30,
-                      height: 30),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text("CPTM METRO",
-                        style: TextStyle(fontSize: 32.0, color: Colors.grey)),
-                  )
-                ],
-              ),
-            ),
-            NewWidget(),
-            NewWidget()
-          ],
-        ) // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Header();
+            } else {
+              return NewWidget();
+            }
+          },
+          itemCount: 3),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: <Widget>[
+          Image.network(
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/CPTM_icon.svg/1024px-CPTM_icon.svg.png",
+              width: 30,
+              height: 30),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text("CPTM METRO",
+                style: TextStyle(fontSize: 32.0, color: Colors.grey)),
+          )
+        ],
+      ),
     );
   }
 }
@@ -58,7 +70,7 @@ class NewWidget extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child: Container(
                 decoration:
-                BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+                    BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
                 height: 60,
                 width: 60,
                 child: Center(
